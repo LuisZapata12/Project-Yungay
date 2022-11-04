b2bc8c7d81e137debd7ec33946a2aab981346f82
bruno Salas <65439485+BrunoSalas@users.noreply.github.com>
2022-11-04 02:14:20 -0500

    stamina

--- My project Yungay/Assets/Scripts/Inventory/UI/Stamina.cs
+++ My project Yungay/Assets/Scripts/Inventory/UI/Stamina.cs
@@ -41,7 +41,7 @@ public class Stamina : MonoBehaviour
 
     public void IsRunning()
     {
-        if (model.actualSpeed >= model.speedRun - 1f && PlayerGroundCheck.grounded)
+        if (model.actualSpeed >= model.speedRun && PlayerGroundCheck.grounded)
         {
             model.staActual -= model.staminaDrain * Time.deltaTime;
             CheckStamina(1);
