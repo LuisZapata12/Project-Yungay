bfa2a7819c6c85d6c0191391a75a9171457fcce7
jorgeluis15520 <103298795+jorgeluis15520@users.noreply.github.com>
2022-11-08 02:00:40 -0500

    CircleBar

--- My project Yungay/Assets/scripts/Cure.cs
+++ My project Yungay/Assets/scripts/Cure.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Cure : MonoBehaviour
 {
     public PlayerHealth playerHealth;
@@ -15,10 +15,16 @@ public class Cure : MonoBehaviour
     public float timer;
     public float maxTime;
     private bool charge;
+    public GameObject chargeBar;
+    public float speedBar;
+    public Image image;
+    private Coroutine coroutine;
 
     private void Start()
     {
         inventoryDisplay = InventoryDisplay.instance;
+        chargeBar.SetActive(false);
+        image = chargeBar.transform.GetChild(0).GetComponent<Image>();
     }
 
     private void Update()
@@ -26,6 +32,7 @@ public class Cure : MonoBehaviour
         if (inventory.CheckItem(bandageItem) && playerHealth.mb.health < playerHealth.mb.maxHealth && Input.GetMouseButtonDown(0)) 
         {
             charge = true;
+            coroutine = StartCoroutine(FillBar());
         }
         else if(Input.GetMouseButtonUp(0))
         {
@@ -34,6 +41,27 @@ public class Cure : MonoBehaviour
                 timer = 0;
                 charge = false;
             }
+
+            if (coroutine != null)
+            {
+                StopCoroutine(coroutine);
+                image.fillAmount = 0;
+                chargeBar.SetActive(false);
+            }
+
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            coroutine = StartCoroutine(FillBar());
+        }
+
+        if (Input.GetMouseButtonUp(0) && coroutine != null)
+        {
+            StopCoroutine(coroutine);
+            image.fillAmount = 0;
+            chargeBar.SetActive(false);
+
         }
 
         if (timer >= maxTime)
@@ -68,5 +96,18 @@ public class Cure : MonoBehaviour
         }
         yield break;
 
+    }
+
+    IEnumerator FillBar()
+    {
+        while(image.fillAmount < 1)
+        {
+            chargeBar.SetActive(true);
+            image.fillAmount += speedBar * Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        chargeBar.SetActive(false);
+        image.fillAmount = 0;
+        yield break;
     }
 }
