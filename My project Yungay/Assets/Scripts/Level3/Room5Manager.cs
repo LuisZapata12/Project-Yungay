60594b3b313cd7aebfb9351ab94adbe76791a77d
George Jimy <103298857+KnivesPeans@users.noreply.github.com>
2022-11-25 22:28:20 -0500

    Update

--- My project Yungay/Assets/Scripts/Level3/Room5Manager.cs
+++ My project Yungay/Assets/Scripts/Level3/Room5Manager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Room5Manager : MonoBehaviour
 {
-    public GameObject SpawnEnemysRoom3;
+    public GameObject spawnEnemysRoom3;
+    public GameObject audio;
+    public TMP_Text textMesh;
+    public float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +24,16 @@ public class Room5Manager : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            SpawnEnemysRoom3.SetActive(true);
+            spawnEnemysRoom3.SetActive(true);
+            audio.SetActive(true);
+            textMesh.text = "Cuidado, puede que este cerca";
+
+            timer += Time.deltaTime;
+            if (timer >= 3)
+            {
+                textMesh.text = "";
+                Destroy(this.gameObject);
+            }
         }
         
     }
