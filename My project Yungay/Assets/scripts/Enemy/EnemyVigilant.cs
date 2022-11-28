734ea36b15877fcbb2d0bb37bf0c4526d4fcd5f0
George Jimy <103298857+KnivesPeans@users.noreply.github.com>
2022-11-28 17:56:16 -0500

    Fix Enemys

--- My project Yungay/Assets/scripts/Enemy/EnemyVigilant.cs
+++ My project Yungay/Assets/scripts/Enemy/EnemyVigilant.cs
@@ -25,6 +25,12 @@ public class EnemyVigilant : MonoBehaviour
     public float waitTime;
     private float timer;
     private Vector3 originalPos;
+    private Vector3 directionToTarget;
+
+    [SerializeField]
+    private TrailRenderer bulletTrail;
+
+    private float BulletSpeed = 100f;
     //public float rangeWeapon;
 
     void Start()
@@ -223,9 +229,9 @@ public class EnemyVigilant : MonoBehaviour
     }
     public void Shoot()
     {
-
+        directionToTarget = (Target.transform.position - transform.position).normalized;
         RaycastHit hit;
-        if  (Physics.Raycast (pointShoot.transform.position,pointShoot.transform.forward,out hit ,Weapon.Range))
+        if (Physics.Raycast(pointShoot.transform.position, directionToTarget, out hit, Weapon.Range))
         {
             if (hit.transform.gameObject.CompareTag("Player"))
             {
@@ -235,17 +241,22 @@ public class EnemyVigilant : MonoBehaviour
                     if (Timer > Weapon.timeToShoot)
                     {
                         anim.SetBool("Shoot", true);
+                        TrailRenderer trail = Instantiate(bulletTrail, pointShoot.transform.position, Quaternion.identity);
+                        StartCoroutine(SpawnTrail(trail, hit.point));
                         Weapon.Munition--;
                         Timer = 0;
                         Life.Damage(Weapon.damage);
 
                     }
                     else
-
                     {
                         anim.SetBool("Shoot", false);
                     }
+
+
                 }
+
+
             }
 
         }
@@ -253,12 +264,31 @@ public class EnemyVigilant : MonoBehaviour
         {
             anim.SetBool("Reload", true);
             Timer += Time.deltaTime;
-            if (Timer>=Weapon.timetoRecharge)
+            if (Timer >= Weapon.timetoRecharge)
             {
-                Weapon.Munition +=Weapon.charger;
+                Weapon.Munition += Weapon.charger;
                 Timer = 0;
                 anim.SetBool("Reload", false);
             }
         }
+    }
+
+    private IEnumerator SpawnTrail(TrailRenderer Trail, Vector3 HitPoint)
+    {
+        Vector3 startPosition = Trail.transform.position;
+        float distance = Vector3.Distance(Trail.transform.position, HitPoint);
+        float remainingDistance = distance;
+
+        while (remainingDistance > 0)
+        {
+            Trail.transform.position = Vector3.Lerp(startPosition, HitPoint, 1 - (remainingDistance / distance));
+
+            remainingDistance -= BulletSpeed * Time.deltaTime;
+
+            yield return null;
+        }
+        Trail.transform.position = HitPoint;
+
+        Destroy(Trail.gameObject, Trail.time);
     }
 }
