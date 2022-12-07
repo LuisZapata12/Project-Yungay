e180ead411afa55ddb5a0ed44e6d04618fbad8f7
George Jimy <103298857+KnivesPeans@users.noreply.github.com>
2022-12-06 22:19:05 -0500

    Update Tutorial cs

--- My project Yungay/Assets/Scripts/TutoEmergente/Tutorial.cs
+++ My project Yungay/Assets/Scripts/TutoEmergente/Tutorial.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System;
+using TMPro;
+
 
 public class Tutorial : MonoBehaviour
 {
     public static Tutorial instance;
 
+    public GameObject Panel;
     public GameObject [] tutorialImag;
+    public string [] tutorialText;
+    public TMP_Text tmpText;
+
+    
     // Start is called before the first frame update
     private void Awake()
     {
@@ -28,8 +34,22 @@ public class Tutorial : MonoBehaviour
 
     }
 
+
+
     public void ShowTutoImage(int x)
     {
-        tutorialImag[x].enabled = true;
+        Panel.SetActive(true);
+        tutorialImag[x].SetActive(true);
+        tmpText.text = tutorialText[x];
+        GameManager.inPause = true;
+        
+    }
+
+    public void SkipTutoImage(int x)
+    {
+        Panel.SetActive(false);
+        tutorialImag[x].SetActive(false);
+        tmpText.text = null;
+        GameManager.inPause = false;
     }
 }
